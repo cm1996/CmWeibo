6de76d9a60ec10ed68cc7a9c29ecdf94d0c58e7d
cm1996 <chenmo1996@outlook.com>
2016-08-24 19:05:04 +0800

    FirstCommit

---
+++ app/src/main/java/ssdut/chenmo/cmweibo/activity/LoginActivity.java
@@ -0,0 +1,133 @@
+package ssdut.chenmo.cmweibo.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sina.weibo.sdk.api.share.Base;
+import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+import com.sina.weibo.sdk.auth.WeiboAuthListener;
+import com.sina.weibo.sdk.auth.sso.SsoHandler;
+import com.sina.weibo.sdk.exception.WeiboException;
+
+import java.text.SimpleDateFormat;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import ssdut.chenmo.cmweibo.R;
+import ssdut.chenmo.cmweibo.config.Constants;
+import ssdut.chenmo.cmweibo.utils.AccessTokenKeeper;
+
+public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.login_btn)
+    Button mLoginButton;
+    @BindView(R.id.token_info_tv)
+    TextView mTokenText;
+
+    private AuthInfo mAuthInfo;
+
+    /** 封装了 "access_token"，"expires_in"，"refresh_token"，并提供了他们的管理功能  */
+    private Oauth2AccessToken mAccessToken;
+
+    /** 注意：SsoHandler 仅当 SDK 支持 SSO 时有效 */
+    private SsoHandler mSsoHandler;
+
+    @Override
+    protected void initData() {
+        //开始微博认证登录
+        mAuthInfo = new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE);
+        //我们使用的是 SSO + WEB 方式
+        mSsoHandler = new SsoHandler(LoginActivity.this, mAuthInfo);
+    }
+
+    @Override
+    protected int initLayoutRes() {
+        return R.layout.activity_login;
+    }
+
+    @OnClick(R.id.login_btn)
+    public void onLoginButtonClick(View v){
+        mSsoHandler.authorize(new AuthListener());
+    }
+
+    // SSO 认证后需要重写这个函数
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (mSsoHandler != null) {
+            mSsoHandler.authorizeCallBack(requestCode, resultCode, data);
+        }
+    }
+
+    //微博认证回调类
+    //1 web授权的结束后会直接 调用这个
+    //2 sso授权的 需要重写上面的 函数 才会回调这个类
+    class AuthListener implements WeiboAuthListener {
+
+        @Override
+        public void onComplete(Bundle values) {
+            // 从 Bundle 中解析 Token
+            mAccessToken = Oauth2AccessToken.parseAccessToken(values);
+            //从这里获取用户输入的 电话号码信息
+            String  phoneNum =  mAccessToken.getPhoneNum();
+            if (mAccessToken.isSessionValid()) {
+                // 显示 Token
+                updateTokenView(false);
+
+                // 保存 Token 到 SharedPreferences
+                AccessTokenKeeper.writeAccessToken(LoginActivity.this, mAccessToken);
+                Toast.makeText(LoginActivity.this,
+                        R.string.weibosdk_demo_toast_auth_success, Toast.LENGTH_SHORT).show();
+            } else {
+                // 以下几种情况，您会收到 Code：
+                // 1. 当您未在平台上注册的应用程序的包名与签名时；
+                // 2. 当您注册的应用程序包名与签名不正确时；
+                // 3. 当您在平台上注册的包名和签名与您当前测试的应用的包名和签名不匹配时。
+                String code = values.getString("code");
+                String message = getString(R.string.weibosdk_demo_toast_auth_failed);
+                if (!TextUtils.isEmpty(code)) {
+                    message = message + "\nObtained the code: " + code;
+                }
+                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onCancel() {
+            Toast.makeText(LoginActivity.this,
+                    R.string.weibosdk_demo_toast_auth_canceled, Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onWeiboException(WeiboException e) {
+            Toast.makeText(LoginActivity.this,
+                    "Auth exception : " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void updateTokenView(boolean hasExisted) {
+        String date = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(
+                new java.util.Date(mAccessToken.getExpiresTime()));
+        String format = getString(R.string.weibosdk_demo_token_to_string_format_1);
+        mTokenText.setText(String.format(format, mAccessToken.getToken(), date));
+
+        String message = String.format(format, mAccessToken.getToken(), date);
+        if (hasExisted) {
+            message = getString(R.string.weibosdk_demo_token_has_existed) + "\n" + message;
+
+        }
+        mTokenText.setText(message);
+    }
+
+    @Override
+    protected void onExit() {
+
+    }
+}
---
+++ app/src/main/java/ssdut/chenmo/cmweibo/activity/WelcomeActivity.java
@@ -0,0 +1,54 @@
+package ssdut.chenmo.cmweibo.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.sina.weibo.sdk.api.share.Base;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
+
+import java.text.SimpleDateFormat;
+
+import ssdut.chenmo.cmweibo.R;
+import ssdut.chenmo.cmweibo.utils.AccessTokenKeeper;
+
+
+/**
+ * 进入APP的第一个界面 欢迎界面
+ * 功能 ：  初始化各种东西 我还没想好
+ *          判断进入哪个页面
+ */
+public class WelcomeActivity extends BaseActivity {
+
+    /** 封装了 "access_token"，"expires_in"，"refresh_token"，并提供了他们的管理功能  */
+    private Oauth2AccessToken mAccessToken;
+
+
+    @Override
+    protected void initData() {
+
+        mAccessToken = AccessTokenKeeper.readAccessToken(this);
+        if (mAccessToken.isSessionValid()) {
+            //上次的登录信息存在且可用  那就直接进入 MainActivity
+            Log.e("1111111111111","11111111111");
+        } else {
+            Log.e("222222222222","2222222222");
+            //不存在或者不可用 进入 LoginActivity
+            startActivity(new Intent(WelcomeActivity.this,LoginActivity.class));
+            finish();
+            playOpenAnimation();
+        }
+    }
+
+
+    @Override
+    protected int initLayoutRes() {
+        return R.layout.activity_welcome;
+    }
+
+    @Override
+    protected void onExit() {
+
+    }
+}
